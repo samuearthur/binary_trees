dccc6ebe15a6e6773ee0ea09d508dd15cb5941da
samuearthur <87957433+samuearthur@users.noreply.github.com>
2022-08-26 02:12:26 +0000

    0x1D. C - Binary trees_task_completed

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,22 +2,25 @@
 
 /**
  * binary_tree_node - creates a binary tree node
- * @parent: pointer to the parent node of the node to create
- * @value: value to put in the new node
  *
- * Return: Pointer to the newly created node
- *         NULL on failure
+ * @parent: pointer to the parent node
+ *
+ * @value: value assigned to the new node
+ *
+ * Return: A new node or NULL if fails.
  */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+	binary_tree_t *created_node = NULL;
 
-	new = malloc(sizeof(binary_tree_t));
-	if (!new)
+	created_node = malloc(sizeof(binary_tree_t));
+	if (!created_node)
 		return (NULL);
-	new->n = value;
-	new->parent = parent;
-	new->left = NULL;
-	new->right = NULL;
-	return (new);
+	created_node->n = value;
+	created_node->parent = parent;
+	created_node->left = NULL;
+	created_node->right = NULL;
+
+	return (created_node);
 }
